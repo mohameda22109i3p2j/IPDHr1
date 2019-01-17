5ae5b37d6e185b0790d09fca83ebc589b3e4f1e1
mohameda22109i3p2j <307883@shakopeeschools.org>
2019-01-17 08:48:43 -0600

    stragety
    created a stragety that betrayed the first round collude the rest

--- team4.py
+++ team4.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Mohamed_ali' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'History'
+strategy_description = 'return collude or betray using their history'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,9 +26,13 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if len(their_history) == 0:
+        return 'b'
+    elif their_history[-1]=='c':
+        return 'c'
+    else:
+        return 'c'
 
-    
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
@@ -44,11 +48,11 @@ def test_move(my_history, their_history, my_score, their_score, result):
             ") returned " + "'" + real_result + "'" +
             " and should have returned '" + result + "'")
         return False
-
-if __name__ == '__main__':
+    History = test_move(their_history)
+    if History == '__main__':
      
     # Test 1: Betray on first move.
-    if test_move(my_history='',
+        if test_move(my_history ='',
               their_history='', 
               my_score=0,
               their_score=0,
